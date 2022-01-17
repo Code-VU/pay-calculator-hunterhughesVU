6e8e63603de28e79701b455bb499d608c76a7ccd
Hunter Hughes <Hunter.Hughes@vu.com>
2022-01-17 14:54:16 -0600

    Completed Assignment

--- payCalculator.py
+++ payCalculator.py
@@ -1,9 +1,16 @@
+
 def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
-    
+    rate = input("Enter Rate:")
+    pay = float(hrs) * float(rate)
+
+    print(pay)
+
+
+
     # end assignment
 
 ## if you want to test locally before you try to sync
